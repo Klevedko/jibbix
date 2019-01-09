fc77248176ce9ebd5df6511c263e46cd77274e5c
akrasilnikov <q21xkr927>
2019-01-09 13:09:01 +0300

    ready for docker-stack use

--- src/main/java/com/jibbix/App.java
+++ src/main/java/com/jibbix/App.java
@@ -18,8 +18,8 @@ public class App {
 
     public static void main(String[] args) {
         logger.debug("start at " + new Date());
-        readPropertiesFromConfigFile();
-        //readPropertiesFromEnvFile();
+        //readPropertiesFromConfigFile();
+        readPropertiesFromEnvFile();
         startWork();
     }
 }
